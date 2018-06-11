03b215fb26c4edddd57fedcbc7e79a07c5d3b9b1
User <user@users-macbook-pro.local>
2018-06-11 20:57:18 +0300

    Initial Commit

---
+++ MyOffers/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MyOffers
+//
+//  Created by User on 11.06.18.
+//  Copyright © 2018 Project. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MyOffers/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  MyOffers
+//
+//  Created by User on 11.06.18.
+//  Copyright © 2018 Project. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
